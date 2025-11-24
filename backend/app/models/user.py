2e18fecdd7dd772f621c9c84fa805643fd994d9b
GanaelD <ganael59320@gmail.com>
2025-11-24 22:32:32 +0100

    feat: create user model class

---
+++ backend/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime, timezone
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: int | None = Field(default=None, primary_key=True)
+    email: str = Field(unique=True, index=True)
+    username: str = Field(unique=True, index=True)
+    hashed_password: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
