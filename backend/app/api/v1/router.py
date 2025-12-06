5151bd8d7ea859c7638d6d16d897d09dd1d1b8fe
gazeden <ganael59320@gmail.com>
2025-12-06 21:48:46 +0100

    feat: include user router in main router

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,3 +1,7 @@
+from app.api.v1.endpoints import users
 from fastapi import APIRouter
 
 api_router = APIRouter()
+
+# Include all endpoint routers
+api_router.include_router(users.router, prefix="/users", tags=["users"])
