787b6cebb3bdfa1a3ef0d90305180787377ad5dd
gazeden <ganael59320@gmail.com>
2025-12-06 22:12:05 +0100

    feat: add new global exception handler

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,26 @@
+import logging
+
 from app.api.v1.router import api_router
 from app.config import settings
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+
+logger = logging.getLogger(__name__)
+
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
 )
+
+
+# Exception handlers
+@app.exception_handler(Exception)
+async def global_exception_handler(request, exc):
+    logger.error(f"Global exception: {exc}", exc_info=True)
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"detail": "Internal server error"},
+    )
 
 
 # Root endpoint
