9a24a5e06c5f459e5b46603947bf7b5370d82bec
gazeden <ganael59320@gmail.com>
2025-12-06 21:39:44 +0100

    feat: add get_current_superuser deps fn

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -54,6 +54,17 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     return current_user
 
 
+async def get_current_superuser(current_user: User = Depends(get_current_user)):
+    """
+    Dependency for getting current superuser.
+    """
+    if not user_crud.is_superuser(current_user):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="The user doesn't have enough privileges",
+        )
+
+
 # Common query parameters for pagination
 class CommonQueryParams:
     """
