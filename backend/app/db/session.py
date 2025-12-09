e6f8ecccd455b816206962a60040dc6740ec3b6f
gazeden <ganael59320@gmail.com>
2025-12-09 22:40:09 +0100

    feat: add init_db func to init db tables [back]

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -7,7 +7,6 @@ from sqlmodel import Session, SQLModel, create_engine
 engine = create_engine(settings.DATABASE_URL, echo=True)
 
 
-@contextmanager
 def get_session():
     with Session(engine) as session:
         yield session
