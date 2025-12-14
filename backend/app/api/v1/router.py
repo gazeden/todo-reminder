2b8c850d2424e146eea2252aaa02de36177d8f73
gazeden <ganael59320@gmail.com>
2025-12-14 22:28:10 +0100

    feat: add task endpoint to main router [back]

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,4 +1,4 @@
-from app.api.v1.endpoints import auth, users
+from app.api.v1.endpoints import auth, tasks, users
 from fastapi import APIRouter
 
 api_router = APIRouter()
@@ -7,3 +7,5 @@ api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+
+api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
